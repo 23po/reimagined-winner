9b7d223df4f0d1368f98d5cacb5d937e4a9885d8
calvin <ckodalo@gmail.com>
2022-11-07 05:01:01 +0300

    added theme file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e6652129e7cffc7d"; }
+/******/ 		__webpack_require__.h = function() { return "08a4d5041484a1dc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
